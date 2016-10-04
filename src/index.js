157083316bfb730907495bd8b2a55094c7d61693
Garry Taylor <giz@gbdesign.net>
2016-10-04 20:38:08 +0100

    Include React-Router requirements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import Router from 'react-router/BrowserRouter'
+import Match from 'react-router/Match'
+import Link from 'react-router/Link'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
