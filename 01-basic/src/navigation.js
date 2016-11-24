6edab6b12aa6d367b2aec9002391c1b8ab29a4de
Garry Taylor <giz@gbdesign.net>
2016-11-24 23:45:11 +0000

    Link Props Include props for active style and activeOnlyWhenExact for the home page.

--- 01-basic/src/navigation.js
+++ 01-basic/src/navigation.js
@@ -4,8 +4,8 @@ import Link from 'react-router/Link';
 var Navigation = () => {
   return <div>
     <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
+      <li><Link to="/" activeStyle={{color:"green"}} activeOnlyWhenExact={true}>Home</Link></li>
+      <li><Link to="/about" activeStyle={{color:"green"}}>About</Link></li>
       <li><Link to="/missing">Missing</Link></li>
     </ul>
   </div>
