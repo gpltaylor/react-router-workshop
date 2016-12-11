267342959f086ec4473003796d4b920dd3112ba8
Garry Taylor <giz@gbdesign.net>
2016-12-11 22:24:30 +0000

    Test Cleanup Clean up the tests to remove the Format helper. Also point to Index directly as I am going to change the data file to a JSON file soon,

--- 02-params/__tests__/App.contact.success.test.js
+++ 02-params/__tests__/App.contact.success.test.js
@@ -5,27 +5,10 @@ import App from '../src/App';
 import Router from 'react-router/MemoryRouter';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 
-// Removed React Data attributes not required for Snaphot testing
-const format = (html) => {
-  function replacer(match, p1, p2, p3, offset, string) {
-    return [p1, p2, p3].join(' - ');
-  }
-// 
-  let data = html.replace(/data\-reactid\=\"[0-9]*\"/g, '');
-  data = data.replace(/data\-react\-checksum\=\"-[0-9]*\"/g, '');
-  data = data.replace(/data\-react\-checksum\=\"[0-9]*\"/g, '');
-  data = data.replace(/\<\!\-\- react\-empty\: [0-9]* \-\-\>/g, '');
-  data = data.replace(/data\-reactroot\=\"\"/g, '');
-  data = data.replace(/\<\!\-\- \/react\-text \-\-\>/g, '');
-  data = data.replace(/\<\!\-\- react\-text\: [0-9]* \-\-\>/g, '');
-  data = data.replace(/ *>/g, '>');
-  return data;
-}
-
 import {
   homeHTML, aboutHTML, applicationHTML, applicationWithNotesHTML, 
   missingHTML, contactHTML, contactSuccessHTML, empty
-} from "./data";
+} from "./data/index";
 
 describe('app.contact.test', () => {
   it('Successfully completed', () => {
